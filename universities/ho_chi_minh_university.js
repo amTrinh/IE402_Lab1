c6c5f77b91b74c04740f1d7c79c220fb98212a4e
TNMyTien <92216495+TNMyTien@users.noreply.github.com>
2023-03-22 09:32:10 +0700

    trường đh ở TPHCM

---
+++ universities/ho_chi_minh_university.js
@@ -0,0 +1,15 @@
+const nguyentatthanh = 
+    {
+        title:"ĐH Nguyễn Tất Thành",
+        address: "300A Nguyễn Tất Thành, Phường 13, Quận 4, TP. Hồ Chí Minh, Việt Nam",
+        longitude: 106.7104006564399, 
+        latitude: 10.7611845732232
+    };
+
+const tonducthang =
+    {
+        title:"ĐH Tôn Đức Thắng",
+        address: "Số 19 Nguyễn Hữu Thọ, Phường Tân Phong, Quận 7, TP. Hồ Chí Minh, Việt Nam",
+        longitude: 106.6998707431153, 
+        latitude: 10.732871657269
+    };
