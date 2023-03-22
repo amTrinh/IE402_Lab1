176850cca790c2d77266babde7237701d3682eac
TNMyTien <92216495+TNMyTien@users.noreply.github.com>
2023-03-22 11:54:22 +0700

    Update ho_chi_minh_university.js

--- universities/ho_chi_minh_university.js
+++ universities/ho_chi_minh_university.js
@@ -1,4 +1,4 @@
-const nguyentatthanh = 
+    const nguyentatthanh = 
     {
         title:"ĐH Nguyễn Tất Thành",
         address: "300A Nguyễn Tất Thành, Phường 13, Quận 4, TP. Hồ Chí Minh, Việt Nam",
@@ -6,10 +6,25 @@ const nguyentatthanh =
         latitude: 10.7611845732232
     };
 
-const tonducthang =
+    const tonducthang =
     {
         title:"ĐH Tôn Đức Thắng",
         address: "Số 19 Nguyễn Hữu Thọ, Phường Tân Phong, Quận 7, TP. Hồ Chí Minh, Việt Nam",
         longitude: 106.6998707431153, 
         latitude: 10.732871657269
+    };
+    const mo =
+    {
+        title:"ĐH Mở",
+        address: "35 – 37 Hồ Hảo Hớn, Phường Cô Giang, Quận 1, TP. HCM",
+        longitude: 106.6923848142029, 
+        latitude: 10.7611755389438
+    };
+
+    const fpt =
+    {
+        title:"ĐH FPT",
+        address: "Lô E2a-7, Đường D1, Đ. D1, Long Thạnh Mỹ, Thành Phố Thủ Đức, TP HCM",
+        longitude: 106.8099204772914,
+        latitude: 10.8408250593081
     };
