39492be8863cf10dda4bb0681b7d9abd328492dc
TNMyTien <92216495+TNMyTien@users.noreply.github.com>
2023-03-26 17:01:42 +0700

    Update ho_chi_minh_university.js

--- universities/ho_chi_minh_university.js
+++ universities/ho_chi_minh_university.js
@@ -28,3 +28,395 @@
         longitude: 106.8099204772914,
         latitude: 10.8408250593081
     };
+
+    const rmit =
+    {
+        title:"ĐH RMIT",
+        address: "",
+        longitude: 106.695995780185,
+        latitude: 10.729360468795
+    };
+
+    const vanlangcschinh =
+    {
+        title:"ĐH Văn Lang Cơ Sở Chính",
+        address: "69/68 Đặng Thùy Trâm, P.13, Q.Bình Thạnh, TP.HCM",
+        longitude: 106.7027242561977,
+        latitude: 10.8281805147989
+    };
+
+    const vanlangcs1 =
+    {
+        title:"ĐH Văn Lang Cơ Sở 1",
+        address: "45 Nguyễn Khắc Nhu, P. Cô Giang, Q.1, TP.HCM",
+        longitude: 106.6933816219863,
+        latitude: 10.7628851110797
+    };
+
+    const vanlangcs2 =
+    {
+        title:"ĐH Văn Lang Cơ Sở 2",
+        address: "233A Phan Văn Trị, P.11, Q. Bình Thạnh, TP.HCM",
+        longitude: 106.6948734391033,
+        latitude: 10.8123633034454
+    };
+
+    const congnghiep =
+    {
+        title:"ĐH Công Nghiệp",
+        address: "",
+        longitude: 106.6869771501071,
+        latitude: 10.8222211234767
+    };
+
+    const kinhte =
+    {
+        title:"ĐH Kinh Tế",
+        address: "",
+        longitude: 106.6947787113037,
+        latitude: 10.7829751437534
+    };
+
+    const yduoc =
+    {
+        title:"ĐH Y Dược",
+        address: "217 Hồng Bàng, Phường 11, Quận 5, TP.HCM",
+        longitude: 106.6633550804206,
+        latitude: 10.7544583429187
+    };
+
+    const luatcs1 =
+    {
+        title:"ĐH Luật Cơ Sở 1",
+        address: "02 Nguyễn Tất Thành, Phường 12, Quận 4, TP. HCM",
+        longitude: 106.7059118775777,
+        latitude: 10.7674275192685
+    };
+
+    const luatcs2 =
+    {
+        title:"ĐH Luật Cơ Sở 2",
+        address: "123 QL13, Hiệp Bình Chánh, Thành Phố Thủ Đức, TP. HCM",
+        longitude: 106.7137611428846,
+        latitude: 10.8299988893797
+    };
+
+    const supham =
+    {
+        title:"ĐH Sư Phạm",
+        address: "",
+        longitude: 106.6822668653169,
+        latitude: 10.7611124505424
+    };
+
+    const saigon =
+    {
+        title:"ĐH Sài Gòn",
+        address: "",
+        longitude: 106.6823557471173,
+        latitude: 10.7599417658669
+    };
+
+    const khtnq5 =
+    {
+        title:"ĐH Khoa Học Tự Nhiên TPHCM",
+        address: "227 Đ. Nguyễn Văn Cừ, Phường 4, Quận 5, Thành phố Hồ Chí Minh, Việt Nam",
+        longitude: 106.6823336828931,
+        latitude: 10.7625100397561
+    };
+
+    const khtntd =
+    {
+        title:"ĐH Khoa Học Tự Nhiên TP. Thủ Đức Cơ Sở Linh Trung",
+        address: "",
+        longitude: 106.7977161153177,
+        latitude: 10.8759258746952
+    };
+
+    const pnt =
+    {
+        title:"ĐH Y Khoa Phạm Ngọc Thạch",
+        address: "86/2 Thành Thái, Quận 10, TP. Hồ Chí Minh",
+        longitude: 106.666187807381,
+        latitude: 10.773225874111
+    };
+
+    const cntp =
+    {
+        title:"ĐH Công Nghiệp Thực Phẩm",
+        address: "140 Lê Trọng Tấn, Phường Tây Thạnh, Quận Tân Phú, TP.HCM",
+        longitude: 106.628643013253,
+        latitude: 10.8067903351537
+    };
+
+    const csndq7 =
+    {
+        title:"ĐH Cảnh Sát Nhân Dân Quận 7",
+        address: "36 Nguyễn Hữu Thọ - Phường Tân Phong - Quận 7 - TP. Hồ Chí Minh",
+        longitude: 106.6979978670938,
+        latitude: 10.7344547397454
+    };
+
+    const csndtd =
+    {
+        title:"ĐH Cảnh Sát Nhân Dân TP. Thủ Đức",
+        address: "179A Kha Vạn Cân - Phường Linh Tây - TP Thủ Đức",
+        longitude: 106.7553742740599,
+        latitude: 10.8589873303698
+    };
+
+    const khxhnvhcm =
+    {
+        title:"ĐH Khoa Học Xã Hội và Nhân Văn TP. HCM",
+        address: "10-12 Đ. Đinh Tiên Hoàng, Bến Nghé, Quận 1, Thành phố Hồ Chí Minh, Việt Nam",
+        longitude: 106.7027750799656,
+        latitude: 10.7858937573751
+    };
+
+    const khxhnvtd =
+    {
+        title:"ĐH Khoa Học Xã Hội và Nhân Văn TP. Thủ Đức",
+        address: "",
+        longitude: 106.8023257527647,
+        latitude: 10.8724762402082
+    };
+
+    const kientruc =
+    {
+        title:"ĐH Kiến Trúc",
+        address: "",
+        longitude: 106.6944724341229,
+        latitude: 10.7819445536455
+    };
+
+    const hocvienhangkhong =
+    {
+        title:"Học Viện Hàng Không Việt Nam",
+        address: "",
+        longitude: 106.6729818451233,
+        latitude: 10.7971850433652
+    };
+
+    const bkcs1 =
+    {
+        title:"ĐH Bách Khoa Cơ Sở 1",
+        address: "268 Lý Thường Kiệt, Phường 14, Quận 10, Thành phố Hồ Chí Minh, Việt Nam",
+        longitude: 106.6595707346623,
+        latitude: 10.7734400031187
+    };
+
+    const bkcs2 =
+    {
+        title:"ĐH Bách Khoa Cơ Sở 2",
+        address: "Đông Hoà, Dĩ An, Bình Dương, Việt Nam",
+        longitude: 106.8049435877811,
+        latitude: 10.8808003088055
+    };
+
+    const nhacvienhcm =
+    {
+        title:"Nhạc Viện TP. HCM",
+        address: "112 Đường Nguyễn Du, Phường Bến Thành, Quận 1, Hồ Chí Minh",
+        longitude: 106.6949912519058,
+        latitude: 10.7743075222262
+    };
+
+    const gtvthcm =
+    {
+        title:"ĐH Giao Thông Vận Tải TP. HCM",
+        address: "02 Võ Oanh, Phường 25, Bình Thạnh, Thành phố Hồ Chí Minh",
+        longitude: 106.717014536897,
+        latitude: 10.8046811413907
+    };
+
+    const phgtvt =
+    {
+        title:"Phân Hiệu Trường Đại Học Giao Thông Vận Tải Tại TP. HCM",
+        address: "451 Lê Văn Việt, Tăng Nhơn Phú A, Thủ Đức, Thành phố Hồ Chí Minh",
+        longitude: 106.7941407777539,
+        latitude: 10.8455932725713
+    };
+
+    const hutechhcm =
+    {
+        title:"ĐH Công Nghệ HUTECH TP. HCM ",
+        address: "475A Đ. Điện Biên Phủ, Phường 25, Bình Thạnh, Thành phố Hồ Chí Minh",
+        longitude: 106.7144076983814,
+        latitude: 10.8015722291364
+    };
+
+    const hutechtd =
+    {
+        title:"ĐH Công Nghệ HUTECH TP. Thủ Đức",
+        address: "10/80C XL Hà Nội, Phường Tân Phú, Thủ Đức, Thành phố Hồ Chí Minh",
+        longitude: 106.7850814857825,
+        latitude: 10.8555691375542
+    };
+
+    const kinhtetaichinh =
+    {
+        title:"ĐH Kinh Tế Tài Chính TP. HCM",
+        address: "145 Đ. Điện Biên Phủ, Phường 15, Bình Thạnh, Thành phố Hồ Chí Minh",
+        longitude: 106.7036029557986,
+        latitude: 10.7973274557845
+    };
+
+    const hoasen =
+    {
+        title:"ĐH Hoa Sen",
+        address: "8 Nguyễn Văn Tráng, Phường Bến Thành, Quận 1, Thành phố Hồ Chí Minh",
+        longitude: 106.6923691348337,
+        latitude: 10.770116248218
+    };
+
+    const mythuat =
+    {
+        title:"ĐH Mỹ Thuật",
+        address: "5 Phan Đăng Lưu, Phường 3, Bình Thạnh, Thành phố Hồ Chí Minh",
+        longitude: 106.6954748644312,
+        latitude: 10.8027078990177
+    };
+
+    const hongbang =
+    {
+        title:"ĐH Quốc Tế Hồng Bàng",
+        address: "215 Đường Điện Biên Phủ, Phường 15, Quận Bình Thạnh, Hồ Chí Minh",
+        longitude: 106.7063667828438,
+        latitude: 10.8002723936524
+    };
+
+    const spktcs1 =
+    {
+        title:"ĐH Sư Phạm Kỹ Thuật TP. HCM",
+        address: "01 Đ. Võ Văn Ngân, Linh Chiểu, Thủ Đức, Thành phố Hồ Chí Minh",
+        longitude: 106.7709895579789,
+        latitude: 10.8501590066838
+    };
+
+    const spktcs2 =
+    {
+        title:"ĐH Sư Phạm Kỹ Thuật Cơ Sở 2",
+        address: "484 Lê Văn Việt, Tăng Nhơn Phú A, Quận 9, Thành phố Hồ Chí Minh",
+        longitude: 106.7963335308206,
+        latitude: 10.8450492184273
+    };
+
+    const nganhang =
+    {
+        title:"ĐH Ngân Hàng",
+        address: "52b Đ. Hoàng Diệu 2, Phường Linh Trung, Thủ Đức, Thành phố Hồ Chí Minh",
+        longitude: 106.7622450196247,
+        latitude: 10.8575179808489
+    };
+
+    const nonglam =
+    {
+        title:"ĐH Nông Lâm TP. HCM",
+        address: "Khu Phố 6, Thủ Đức, Thành phố Hồ Chí Minh",
+        longitude: 106.7888682939951,
+        latitude: 10.8697893061223
+    };
+
+    const ktluat =
+    {
+        title:"ĐH Kinh Tế Luật TP. HCM",
+        address: "669 QL1A, khu phố 3, Thủ Đức, Thành phố Hồ Chí Minh",
+        longitude: 106.7784872497826,
+        latitude: 10.8708835460938
+    };
+
+    const tdtt =
+    {
+        title:"ĐH Thể Dục Thể Thao TP. HCM",
+        address: "26 Chương Dương, Linh Chiểu, Thủ Đức, Thành phố Hồ Chí Minh",
+        longitude: 106.7958625968343,
+        latitude: 10.8719591252612
+    };
+
+    const quocte =
+    {
+        title:"ĐH Quốc Tế IU",
+        address: "khu phố 6, TP Thủ Đức, Thành phố Hồ Chí Minh",
+        longitude: 106.8016087879619,
+        latitude: 10.8776521653042
+    };
+
+    const anninhnd =
+    {
+        title:"ĐH An Ninh Nhân Dân",
+        address: "Km18 XL Hà Nội, Phường Linh Trung, Thủ Đức, Thành phố Hồ Chí Minh",
+        longitude: 106.807476788606,
+        latitude: 10.8734299152367
+    };
+
+    const phthuyloi =
+    {
+        title:"ĐH Thủy Lợi Phân Hiệu Miền Nam",
+        address: "2 Trường Sa, Phường 17, Bình Thạnh, Thành phố Hồ Chí Minh",
+        longitude: 106.706107706232,
+        latitude: 10.792833216775
+    };
+
+    const trandainghia =
+    {
+        title:"ĐH Trần Đại Nghĩa",
+        address: "163 Tân Thới Hiệp 20, Tân Thới Hiệp, Quận 12, Thành phố Hồ Chí Minh",
+        longitude: 106.638071805332,
+        latitude: 10.8618119923271
+    };
+
+    const taichinhmarketing =
+    {
+        title:"ĐH Tài Chính Marketing",
+        address: "27 Tân Mỹ, Tân Thuận Tây, Quận 7, Thành phố Hồ Chí Minh",
+        longitude: 106.719860338228,
+        latitude: 10.7522385902626
+    };
+
+    const tnmt =
+    {
+        title:"ĐH Tài Nguyên và Môi Trường TP. HCM",
+        address: "236 Đ. Lê Văn Sỹ, Phường 1, Tân Bình, Thành phố Hồ Chí Minh",
+        longitude: 106.6668712505506,
+        latitude: 10.7965880208993
+    };
+
+    const vanhoacs1 =
+    {
+        title:"ĐH Văn Hóa Cơ Sở 1",
+        address: "51 Đ. Quốc Hương, Thảo Điền, Quận 2, Thành phố Hồ Chí Minh 700000, Việt Nam",
+        longitude: 106.7311803503124,
+        latitude: 10.8054544142938
+    };
+
+    const vanhoacs2 =
+    {
+        title:"ĐH Văn Hóa Cơ Sở 2",
+        address: "Phước Long B, Quận 9, Thành phố Hồ Chí Minh",
+        longitude: 106.770014263662,
+        latitude: 10.8234825742625
+    };
+
+    const hungvuong =
+    {
+        title:"ĐH Hùng Vương",
+        address: "28-30 Ngô Quyền, Phường 6, Quận 5, Thành phố Hồ Chí Minh",
+        longitude: 106.6669898084224,
+        latitude: 10.751811664081
+    };
+
+    const greenwich =
+    {
+        title:"ĐH Greenwich Việt Nam",
+        address: "20 Đ. Cộng Hòa, Phường 12, Tân Bình, Thành phố Hồ Chí Minh",
+        longitude: 106.6529640894888,
+        latitude: 10.8036804271668
+    };
+
+    const vanhien =
+    {
+        title:"ĐH Văn Hiến",
+        address: "128 Phạm Văn Hai, Phường 3, Tân Bình, Thành phố Hồ Chí Minh",
+        longitude: 106.6626790887955,
+        latitude: 10.793568179995
+    };
